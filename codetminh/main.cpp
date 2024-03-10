1d4cded3485fed3f526234e8ae4fa846698d9abd
tuosminh <llttminh@gmail.com>
2024-03-10 16:18:09 +0700

    Initial Commit

---
+++ codetminh/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  codetminh
+//
+//  Created by mac tminhh on 10/03/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
